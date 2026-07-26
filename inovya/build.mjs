dfa20a1f506c2840db3e1de90ada978a433b16a6
Nicolas Lauquin <nlauquin@gmail.com>
2026-07-26 22:32:09 +0200

    Refonte du site Inovya

--- inovya/build.mjs
+++ inovya/build.mjs
@@ -1,10 +1,7 @@
 import { cp, mkdir, rm } from "node:fs/promises";
 
 await rm("dist", { recursive: true, force: true });
-await mkdir("dist/server", { recursive: true });
-await mkdir("dist/assets", { recursive: true });
-await cp("index.html", "dist/assets/index.html");
-await cp("robots.txt", "dist/assets/robots.txt");
-await cp(".htaccess", "dist/assets/.htaccess");
-await cp("og-consulting-capitalized.png", "dist/assets/og-consulting-capitalized.png");
-await cp("worker.js", "dist/server/index.js");
+await mkdir("dist", { recursive: true });
+await cp("index.html", "dist/index.html");
+await cp("robots.txt", "dist/robots.txt");
+await cp(".htaccess", "dist/.htaccess");
